a4d00260f95ff5744d0fd695d1e1516a90e3b67c
seanoughton <seanoughton@gmail.com>
2018-08-20 12:33:04 -0700

    installed redux thunk

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
-//import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 //import thunk from 'redux-thunk';
 //import rootReducer from './reducers';
 
